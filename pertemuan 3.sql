31eef36eecdda955667e2fa0126b6b597063d578
Aisyah Nurul Ihsan <151369624+aishahi@users.noreply.github.com>
2023-11-29 13:25:36 +0700

    Add files via upload

---
+++ pertemuan 3.sql
@@ -0,0 +1,86 @@
+CREATE TABLE mahasiswa (
+nama varchar(20),
+nim integer (15),
+asal varchar (20)
+)
+
+insert into mahasiswa (
+nama,nim,asal
+)
+values('Hilda Nurul',2100015055,'Isekai')
+
+
+insert into mahasiswa (
+nama,nim,asal
+)
+values('Najwa A',2115015047,'Jepang')
+
+
+insert into mahasiswa (
+nama,nim,asal
+)
+values('Nabila',2200015034,'rahim')
+
+
+insert into mahasiswa (
+nama,nim,asal
+)
+values('Mutiara',2100015028,'Aussie')
+
+
+insert into mahasiswa (
+nama,nim,asal
+)
+values('Harni',2115015028,'USA')
+
+
+insert into mahasiswa (
+nama,nim,asal
+)
+values('Wildan',2100015049,'lowanu')
+
+
+insert into mahasiswa (
+nama,nim,asal
+)
+values('Frenky',2200015037,'Imogiri')
+
+
+insert into mahasiswa (
+nama,nim,asal
+)
+values('Agung',2200015037,'Mbantul')
+
+
+insert into mahasiswa (
+nama,nim,asal
+)
+values('Hana',2200015015,'Karawang')
+
+
+insert into mahasiswa (
+nama,nim,asal
+)
+values('Titin',2200015026,'Jepara')
+
+insert into mahasiswa (
+nama,nim,asal
+)
+values('Didi',2200015012,'Kotagede')
+
+
+insert into mahasiswa (
+nama,nim,asal
+)
+values('Atta',2200015011,'Banguntapan')
+
+
+insert into mahasiswa (
+nama,nim,asal
+)
+values('Maya',2200015023,'Pleret')
+
+
+
+select * FROM mahasiswa m
+
